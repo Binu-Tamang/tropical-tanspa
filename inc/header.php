d9c4c17b7d8e5ac56452e12790af3b1fd6cf2344
Binu_Tamang <137280783+Binu-Tamang@users.noreply.github.com>
2024-01-05 17:01:36 +0545

    first commit

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav class="nav-menu"></nav>
+    <label for="check">
+      <input type="checkbox" id="check"/> 
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- font awesome,custom css,bootstrap css -->
+    <?php include('inc/css.php') ?>
+
+
+    <!-- favicon link -->
+    <link rel="icon" type="images/png" href="">
+    <title>Tropical Tanspa </title>
+</head>
+<body>
+    <!-- header parts start -->
+    <?php include('inc/header.php') ?>
+    <!-- header part ends -->
+</body>
+</html>
